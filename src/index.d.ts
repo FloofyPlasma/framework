7dc9fef8eb9846ccc573d1add77524118805d320
FloofyPlasma <github@floofyplasma.com>
2025-01-21 22:19:26 -0500

    feat: update to mirror upstream

--- src/index.d.ts
+++ src/index.d.ts
@@ -10,6 +10,12 @@ declare namespace Framework {
         Start?: () => void
     }
 
+    export type Lifecycle<T extends any[]> = {
+        Name: string
+        Fire: (self: Lifecycle<T>, ...args: T) => void;
+        Listeners: Array<(...args: T) => void>;
+    }
+
     export function Start(): void;
     export function Add(dirs: Array<Instance>, filter?: (module: ModuleScript) => boolean): void;
     export function New<T>(members: Provider<T>): T;
